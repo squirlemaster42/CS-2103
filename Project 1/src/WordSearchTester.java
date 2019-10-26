0ba960ad8f11f52f9cb942ec38aac5dc1a251f81
Jakob Misbach <jakobmisbach8@gmail.com>
2019-10-26 19:01:22 -0400

    Fixed bugs

--- Project 1/src/WordSearchTester.java
+++ Project 1/src/WordSearchTester.java
@@ -129,7 +129,7 @@ public class WordSearchTester {
 
 		int[][] coord = {{0,0,0},
 						 {2,2,2}};
-		assertArrayEquals(coord ,_wordSearch.search(testGrid,"avw"));
+		assertArrayEquals(coord, _wordSearch.search(testGrid,"avw"));
 	}
 
 	@Test
@@ -162,7 +162,7 @@ public class WordSearchTester {
 									{'p', 's', 'j'}}
 		};
 		int[][] coord = {{1, 2, 1},
-						 {4, 2, 1}};
+						 {3, 2, 1}};
 		assertArrayEquals(coord,_wordSearch.search(testGrid,"svg"));
 	}
 	@Test
